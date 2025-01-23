03867029f7158f13638fdc62814029ff879ffcd3
scrumbler <irishmjn@gmail.com>
2025-01-23 23:09:11 +0545

    Creation of form

--- hospital/views.py
+++ hospital/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from .form import Information
 
 # Create your views here.
 def patient_list(request):
@@ -12,5 +13,16 @@ def patient_list(request):
 def patient_detail(request, pk):
     patient = get_object_or_404(Patient, pk=pk)
     return render(request, 'patient_detail.html', {'patient': patient})
+
+def patient_create(request): #to create the patient if anyone arrives
+    if request.method == 'POST': #using the post method 
+        form = Information(request.POST) #if post method is used it sends data to server
+        if form.is_valid(): #if form contains the name number and gender then form is valid
+            form.save()#form valid == form.save()
+            return redirect('patient_list')
+        else:
+            form = Information()
+            return render(request, 'patient_form.html', {'form': form})
+            
 
 
